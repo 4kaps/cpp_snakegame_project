332ed2529055197580c97b89a956e68df3382fc1
4kaps <100252987+4kaps@users.noreply.github.com>
2023-06-01 13:12:22 +0900

    Posion.hpp

---
+++ src/Poison.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <ncurses.h>
+#include "Drawable.hpp"
+
+class Poison : public Drawable {
+public:
+    Poison(int y, int x) {
+        this->y = y;
+        this->x = x;
+        this->icon = '-';
+    }
+};
