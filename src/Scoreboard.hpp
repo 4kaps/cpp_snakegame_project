680e52bc163ed2dcf05c21a0440168fff2ebb1b2
4kaps <100252987+4kaps@users.noreply.github.com>
2023-06-15 21:55:05 +0900

    Update Scoreboard.hpp
    무빙 거리를 추가하였습니다.

--- src/Scoreboard.hpp
+++ src/Scoreboard.hpp
@@ -1,33 +1,40 @@
 #pragma once
-#include<ncurses.h>
+#include <ncurses.h>
 
 class Scoreboard {
     WINDOW *score_win;
+    WINDOW *time_win;
 public:
-    Scoreboard() {
-        
-    }
+    Scoreboard() {}
 
     Scoreboard(int width, int y, int x) {
         score_win = newwin(1, width, y, x);
+        time_win = newwin(1, width, y + 1, x); 
     }
 
     void initialize(int initial_score) {
         clear();
         mvwprintw(score_win, 0, 0, "Score:");
         updateScore(initial_score);
+        mvwprintw(time_win, 0, 0, "Distance:"); 
         refresh();
     }
 
-    void updateScore(int score) { // 튜토리얼12 07:36
-        mvwprintw(score_win, 0, score_win->_maxx-10, "%11llu", score);
+    void updateScore(int score) {
+        mvwprintw(score_win, 0, score_win->_maxx - 10, "%11llu", score);
+    }
+
+    void updateTime(int time) { 
+        mvwprintw(time_win, 0, time_win->_maxx - 10, "%11llu", time);
     }
 
     void clear() {
         wclear(score_win);
+        wclear(time_win);
     }
 
     void refresh() {
         wrefresh(score_win);
+        wrefresh(time_win);
     }
 };
