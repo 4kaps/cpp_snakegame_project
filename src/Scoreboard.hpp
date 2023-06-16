2a6df27b60fc2b3796bc5e8f1b23234f6d56c513
4kaps <100252987+4kaps@users.noreply.github.com>
2023-06-16 14:50:14 +0900

    Update Scoreboard.hpp

--- src/Scoreboard.hpp
+++ src/Scoreboard.hpp
@@ -4,19 +4,25 @@
 class Scoreboard {
     WINDOW *score_win;
     WINDOW *time_win;
+    WINDOW *speed_win;
+    WINDOW *out_line;
 public:
     Scoreboard() {}
 
     Scoreboard(int width, int y, int x) {
-        score_win = newwin(1, width, y, x);
-        time_win = newwin(1, width, y + 1, x); 
+        score_win = newwin(1, width, y - 13, x + 38);
+        time_win = newwin(1, width, y - 12, x + 38);
+        speed_win = newwin(1, width, y - 11, x + 38);
+        out_line = newwin(5, width + 3, y - 14, x + 37);
     }
 
     void initialize(int initial_score) {
         clear();
+        box(out_line, 0, 0);
         mvwprintw(score_win, 0, 0, "Score:");
         updateScore(initial_score);
         mvwprintw(time_win, 0, 0, "Distance:"); 
+        mvwprintw(speed_win, 0, 0, "Speed:"); 
         refresh();
     }
 
@@ -28,13 +34,21 @@ public:
         mvwprintw(time_win, 0, time_win->_maxx - 10, "%11llu", time);
     }
 
+    void updateSpeed(double speed){
+        mvwprintw(speed_win, 0, speed_win->_maxx - 10, "%11.2lf", speed);
+    }
+
     void clear() {
+        wclear(out_line);
         wclear(score_win);
         wclear(time_win);
+        wclear(speed_win);
     }
 
     void refresh() {
+        wrefresh(out_line);
         wrefresh(score_win);
         wrefresh(time_win);
+        wrefresh(speed_win);
     }
 };
